d80971af3c536bb77a249ac57e01f2c13e4980c2
Ruan Araujo <ruanloking@gmail.com>
2017-06-08 21:37:33 -0300

    Menu lateral diferente. Tela Descubra Pronta, Post também, Sobre também, bug que as vezes não carrega a listview da tela descubra. (Commit anterior não funcionou)

---
+++ euanon/euanon/euanon/Model/Util.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace euanon.Model
+{
+    public class Util
+    {
+        public string Imagem { get; set; }
+        public string selecionaImagem(string categoria)
+        {
+            if (categoria == "Amor")
+                Imagem = "heart.png";
+            else if (categoria == "Carinho")
+                Imagem = "afeicao.png";
+            else if (categoria == "Reflexao")
+                Imagem = "reflexao.png";
+            else if (categoria == "Amizade")
+                Imagem = "amizade.png";
+            return Imagem;
+        }
+    }
+}
---
+++ euanon/euanon/euanon/View/CategoriaPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace euanon.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CategoriaPage : ContentPage
+    {
+        public CategoriaPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
